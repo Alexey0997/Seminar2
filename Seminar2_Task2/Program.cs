c5f4f92502b8604cfc4711a5dc85c8669d597333
alexey <asivlev@yandex.ru>
2022-10-18 22:04:12 +0300

    The square of the number

---
+++ Seminar2_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  По двум заданным числам проверить является ли одно квадратом другого.
+Console.Clear();
+Console.Write("Уважаемый пользователь, введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+if(number1 == number2 * number2)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+if(number2 == number1 * number1)
+{
+    Console.WriteLine($"Число {number2} является квадратом числа {number1}");
+}
+if(number2 != number1 * number1 && number1 != number2 * number2)
+{
+    Console.WriteLine($"Одно число не является квадратом другого.");
+}
