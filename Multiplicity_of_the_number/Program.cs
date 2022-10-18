c055d44a1d3ba5204291042f28b1c61f22d4824c
alexey <asivlev@yandex.ru>
2022-10-18 16:41:40 +0300

    Multiplicity of 7 and 23

---
+++ Multiplicity_of_the_number/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить кратность числа 7 и 23.
+Console.Clear();
+Console.WriteLine("Уважаемый пользователь, введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number%7 == 0 && number%23 == 0)
+{
+    Console.WriteLine($"Уважаемый пользователь число {number} кратно 7 и 23.");
+}
+else
+{
+     Console.WriteLine($"Уважаемый пользователь число {number} некратно 7 и 23.");
+}
