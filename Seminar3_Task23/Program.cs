f35382d62e028588e2c8cb55606af2a72849fde6
alexey <asivlev@yandex.ru>
2022-10-20 16:57:11 +0300

    cube table

---
+++ Seminar3_Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает
+// таблицу кубов чисел от 1 до N. Например, 3-> 1, 8, 27.
+Console.Clear();
+Console.WriteLine("Уважаемый пользователь, введите целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+Console.WriteLine($"Таблица кубов от 1 до {number}: ");
+while(count <= number)
+{
+    int result = count * count * count;
+    Console.Write($"{result}, ");
+    count++;
+}
